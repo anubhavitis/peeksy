3feb9f43da6f7c6d9b0bdfd7310120ff2f8692b0
Anubhav Singhal <ss.lfsgd@gmail.com>
2025-06-16 18:09:13 +0530

    updated dir fetching

--- src/daemon/peeksy.rs
+++ src/daemon/peeksy.rs
@@ -7,6 +7,7 @@ use std::{
     },
 };
 
+use anyhow::Error;
 use chrono::Local;
 use log::{error, info};
 use notify::{Event, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
@@ -26,27 +27,56 @@ fn get_controller() -> Result<SSController, String> {
     Ok(SSController::new(openai_api_key, prompt))
 }
 
+fn get_clean_path(raw: String, home: PathBuf) -> Result<PathBuf, Error> {
+    if raw.starts_with("~/") {
+        return Ok(home.join(raw[2..].to_string()));
+    }
+
+    let raw_path = PathBuf::from(raw.clone());
+    // if raw_path has home path, then return raw_path
+    if raw_path.starts_with(&home) {
+        return Ok(raw_path);
+    } else {
+        return Err(anyhow::anyhow!(
+            "Raw path {} is not understanding",
+            raw_path.display()
+        ));
+    }
+}
+
 pub fn get_screenshot_dir() -> PathBuf {
     use std::process::Command;
-    let output = Command::new("defaults")
+
+    // picks desktop dir if default is not found
+    let desktop_ss_dir = dirs::desktop_dir().unwrap_or_else(|| PathBuf::from("/Users/Shared"));
+
+    let default_dir = Command::new("defaults")
         .args(["read", "com.apple.screencapture", "location"])
         .output()
         .ok();
 
-    if let Some(out) = output {
-        if out.status.success() {
-            let raw = String::from_utf8_lossy(&out.stdout).trim().to_string();
-            let parent = dirs::home_dir().unwrap();
-            // raw is generally like : ~/lorem/ipsum
-            // transforming it into => $home/lorem/ipsum
-            return parent.join(raw[2..].to_string());
-        }
+    dbg!(&default_dir);
+
+    let out = match default_dir {
+        None => return desktop_ss_dir,
+        Some(out) => out,
+    };
+
+    if !out.status.success() {
+        return desktop_ss_dir;
     }
 
-    // picks desktop dir if default is not found
-    let ss_dir = dirs::desktop_dir().unwrap_or_else(|| PathBuf::from("/Users/Shared"));
-    info!("Peeksy: ScreenShot Monitoring directory: {:?}", ss_dir);
-    ss_dir
+    let raw = String::from_utf8_lossy(&out.stdout).trim().to_string();
+    let parent = dirs::home_dir().unwrap();
+    let clean_path = get_clean_path(raw, parent);
+
+    match clean_path {
+        Ok(path) => path,
+        Err(e) => {
+            error!("Error getting clean path: {:?}", e);
+            desktop_ss_dir
+        }
+    }
 }
 
 pub async fn controller(shutdown: Arc<AtomicBool>) {
